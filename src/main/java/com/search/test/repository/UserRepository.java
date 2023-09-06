a2e940cee424cefd38151c2e121fbbcc7b327089
Umjiseung <eom071005123@gmail.com>
2023-09-06 20:47:00 +0900

    Repository 수정

--- src/main/java/com/search/test/repository/UserRepository.java
+++ src/main/java/com/search/test/repository/UserRepository.java
@@ -2,23 +2,15 @@ package com.search.test.repository;
 
 
 import org.apache.catalina.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
 
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User,Long> {
 
-    private static List<User> users;
 
-    public Map<String, String> getText() {
-
-    }
-
-    public List<User> getUser() {
-
-        return this.users;
-    }
 
 }
