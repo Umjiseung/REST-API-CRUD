af152e156927dbcf4971331ad0bf7549d04c6cb2
Umjiseung <eom071005123@gmail.com>
2023-09-26 10:21:01 +0900

    User

---
+++ src/main/java/com/search/test/user/dto/request/RegisterRequest.java
@@ -0,0 +1,12 @@
+package com.search.test.user.dto.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class RegisterRequest<V> {
+
+
+
+}
---
+++ src/main/java/com/search/test/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.search.test.user.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+}
